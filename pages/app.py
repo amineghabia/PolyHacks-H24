41592e7c56d09db1fe74778f5be8e97157027759
Jad RABHI <65916216+EbonGit@users.noreply.github.com>
2024-02-03 18:31:30 -0500

    Update app.py

--- pages/app.py
+++ pages/app.py
@@ -5,10 +5,53 @@ from torch.utils.data import random_split
 import torchvision.models as models
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision.datasets import ImageFolder
+import torchvision.transforms as transforms
 
 import streamlit as st
 from PIL import Image
 import matplotlib.pyplot as plt
+
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+class ImageClassificationBase(nn.Module):
+    def training_step(self, batch):
+        images, labels = batch 
+        out = self(images)                  # Generate predictions
+        loss = F.cross_entropy(out, labels) # Calculate loss
+        return loss
+    
+    def validation_step(self, batch):
+        images, labels = batch 
+        out = self(images)                    # Generate predictions
+        loss = F.cross_entropy(out, labels)   # Calculate loss
+        acc = accuracy(out, labels)           # Calculate accuracy
+        return {'val_loss': loss.detach(), 'val_acc': acc}
+        
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+    
+    def epoch_end(self, epoch, result):
+        print("Epoch {}: train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch+1, result['train_loss'], result['val_loss'], result['val_acc']))
+
+class ResNet(ImageClassificationBase):
+    def __init__(self):
+        super().__init__()
+        # Use a pretrained model
+        self.network = models.resnet50(weights='ResNet50_Weights.DEFAULT')
+        # Replace last layer
+        num_ftrs = self.network.fc.in_features
+        self.network.fc = nn.Linear(num_ftrs, len(dataset.classes))
+    
+    def forward(self, xb):
+        return torch.sigmoid(self.network(xb))
 
 def main():
     st.title("Image Upload and Display App")
